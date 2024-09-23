6d80fbb0bc63680cf82a8758efdb8659f0320375
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-09-23 21:15:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 17.59 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String strAnswer = "";
+        String[] numWords = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        List<String> list = Arrays.asList(numWords);
+        String temp = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            String str = Character.toString(s.charAt(i));
+            
+            if (isInteger(str)) strAnswer += str;
+            else temp += str;
+            
+            if (list.indexOf(temp) > -1) {
+                strAnswer += list.indexOf(temp);
+                temp = "";
+            }
+        }
+        if (temp != "") strAnswer += list.indexOf(temp);
+        
+        answer = Integer.parseInt(strAnswer);
+        return answer;
+    }
+    
+    // 문자열이 정수인지 확인하는 메서드
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);  // 문자열을 정수로 변환
+            return true;            // 변환 성공 시 true 반환
+        } catch (NumberFormatException e) {
+            return false;           // 변환 실패 시 false 반환
+        }
+    }
+    
+}
