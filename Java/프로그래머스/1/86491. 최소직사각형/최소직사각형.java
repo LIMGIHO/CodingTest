0c099270993c013d666b0a0e6274b82d91a92f98
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-09-24 20:27:37 +0900

    Update 최소직사각형.java

--- Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -17,3 +17,24 @@ class Solution {
         return answer;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         return Arrays.stream(sizes).reduce((a, b) -> new int[]{
+//                 Math.max(Math.max(a[0], a[1]), Math.max(b[0], b[1])), Math.max(Math.min(a[0], a[1]), Math.min(b[0], b[1]))
+//         }).map(it -> it[0] * it[1]).get();
+//     }
+// }
+
+// class Solution {
+//     public int solution(int[][] sizes) {
+//         int length = 0, height = 0;
+//         for (int[] card : sizes) {
+//             length = Math.max(length, Math.max(card[0], card[1]));
+//             height = Math.max(height, Math.min(card[0], card[1]));
+//         }
+//         int answer = length * height;
+//         return answer;
+//     }
+// }
