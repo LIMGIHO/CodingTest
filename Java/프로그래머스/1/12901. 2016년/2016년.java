16ce68eb07b3f427cea9f5577c748a8a7671fa3f
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-09-25 20:28:17 +0900

    Update 2016년.java

--- Java/프로그래머스/1/12901. 2016년/2016년.java
+++ Java/프로그래머스/1/12901. 2016년/2016년.java
@@ -11,3 +11,22 @@ class Solution {
         return answer;
     }
 }
+
+
+// import java.util.*;
+
+// class TryHelloWorld
+// {
+//     public String getDayName(int month, int day)
+//     {
+//         Calendar cal = new Calendar.Builder().setCalendarType("iso8601")
+//                         .setDate(2016, month - 1, day).build();
+//         return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, new Locale("ko-KR")).toUpperCase();
+//     }
+//     public static void main(String[] args)
+//     {
+//         TryHelloWorld test = new TryHelloWorld();
+//         int a=5, b=24;
+//         System.out.println(test.getDayName(a,b));
+//     }
+// }
