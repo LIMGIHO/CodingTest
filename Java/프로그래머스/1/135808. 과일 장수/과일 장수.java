c5ffaeb707800bb9b270ed004cd277f1322663e0
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-09-30 22:48:58 +0900

    Update 과일 장수.java

--- Java/프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ Java/프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -17,3 +17,20 @@ class Solution {
         return answer;
     }
 }
+
+
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int k, int m, int[] score) {
+//         int answer = 0;
+
+//         Arrays.sort(score);
+
+//         for(int i = score.length; i >= m; i -= m){
+//             answer += score[i - m] * m;
+//         }
+
+//         return answer;
+//     }
+// }
