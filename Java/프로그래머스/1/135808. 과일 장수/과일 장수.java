67a51022f3653daaa49146c05c67e5596d71842b
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-09-30 22:46:43 +0900

    [level 1] Title: 과일 장수, Time: 495.79 ms, Memory: 146 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Integer[] boxedScore = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxedScore, Comparator.reverseOrder());
+        score = Arrays.stream(boxedScore).mapToInt(Integer::intValue).toArray();
+        for (int i = 0; i < score.length; i+=m) {
+            if (i+m > score.length) break;
+            int[] arr = Arrays.copyOfRange(score, i, i+m);
+            // System.out.println("====" + i + "/" + Arrays.toString(arr));
+            answer += arr[arr.length-1] * arr.length;
+        }
+        
+        // System.out.println(Arrays.toString(score));
+        return answer;
+    }
+}
