e204fcdc2f59a471777cf742fc3da92f5a593815
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-10-01 20:45:19 +0900

    Update 덧칠하기.java

--- Java/프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ Java/프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -26,3 +26,17 @@ class Solution {
         return answer;
     }
 }
+
+
+// class Solution {
+//     public int solution(int n, int m, int[] section) {
+//         int maxPainted = 0, cntPaint = 0;
+//         for (int point : section) {
+//             if (maxPainted <= point) {
+//                 maxPainted = point + m;
+//                 cntPaint++;
+//             }
+//         }
+//         return cntPaint;
+//     }
+// }
