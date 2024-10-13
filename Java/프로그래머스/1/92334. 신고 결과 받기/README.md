43fdf166e4dda8dbacd79532b590883ccecc43ed
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-10-13 20:41:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 5611.46 ms, Memory: 509 MB -BaekjoonHub

--- Java/프로그래머스/1/92334. 신고 결과 받기/README.md
+++ Java/프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 214.63 ms
+메모리: 509 MB, 시간: 5611.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 13일 20:38:17
+2024년 10월 13일 20:41:16
 
 ### 문제 설명
 
--- Java/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ Java/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,35 +1,59 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(String[] id_list, String[] report, int k) {
+//         int[] answer = new int[id_list.length];
+//         Map<String, Set<String>> reportMap = new LinkedHashMap<>();
+//         Map<String, Integer> seqMap = new HashMap<>();
+//         int i = 0;
+//         for (String name : id_list) {
+//             reportMap.put(name, new HashSet<>());
+//             seqMap.put(name, i);
+//             i++;
+//         }
+        
+//         for (String str : report) {
+//             String from = str.split(" ")[0];
+//             String to = str.split(" ")[1];
+            
+//             reportMap.get(to).add(from);
+//         }
+        
+//         for (Map.Entry<String, Set<String>> entry : reportMap.entrySet()) {
+//             // System.out.println(entry.getKey() + " : " + entry.getValue());  
+//             if (entry.getValue().size() >= k) {
+//                 // answer[seqMap.get(entry.getKey())]++;
+                
+//                 for (String name : entry.getValue()) {
+//                     answer[seqMap.get(name)]++;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
-        Map<String, Set<String>> reportMap = new LinkedHashMap<>();
-        Map<String, Integer> seqMap = new HashMap<>();
-        int i = 0;
-        for (String name : id_list) {
-            reportMap.put(name, new HashSet<>());
-            seqMap.put(name, i);
-            i++;
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String, Integer> count = new HashMap<>();
+        for (String s : list) {
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target, 0) + 1);
         }
-        
-        for (String str : report) {
-            String from = str.split(" ")[0];
-            String to = str.split(" ")[1];
-            
-            reportMap.get(to).add(from);
-        }
-        
-        for (Map.Entry<String, Set<String>> entry : reportMap.entrySet()) {
-            // System.out.println(entry.getKey() + " : " + entry.getValue());  
-            if (entry.getValue().size() >= k) {
-                // answer[seqMap.get(entry.getKey())]++;
-                
-                for (String name : entry.getValue()) {
-                    answer[seqMap.get(name)]++;
-                }
-            }
-        }
-        
-        return answer;
+
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        }).mapToInt(Long::intValue).toArray();
     }
 }
