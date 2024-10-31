4963b5e2f259f9365328cbee6cb90843d7927c3f
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-10-31 21:10:57 +0900

    Update 다음 큰 숫자.java

--- Java/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ Java/프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -12,3 +12,15 @@ class Solution {
         return n;
     }
 }
+
+
+class TryHelloWorld {
+    public int nextBigNumber(int n) {
+        int postPattern = n & -n, smallPattern = ((n ^ (n + postPattern)) / postPattern) >> 2;
+        return n + postPattern | smallPattern;
+    }
+    public static void main(String[] args) {
+        int n = 78;
+        System.out.println(new TryHelloWorld().nextBigNumber(n));
+    }
+}
