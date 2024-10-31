dc81d8e6c21622c3ab64f361cf8a001731834287
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2024-10-31 20:09:42 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT CONCAT('/home/grep/src/', T1.BOARD_ID, '/', T1.FILE_ID, T1.FILE_NAME, T1.FILE_EXT) AS FILE_PATH
+  FROM USED_GOODS_FILE T1
+ INNER JOIN (
+            SELECT BOARD_ID
+              FROM USED_GOODS_BOARD
+             ORDER BY VIEWS DESC
+             LIMIT 1
+            ) T2
+    ON T2.BOARD_ID = T1.BOARD_ID
+ ORDER BY T1.FILE_ID DESC
+    ;
