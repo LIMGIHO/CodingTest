d7ffe18ea1b6c917c22928aaaa0d60235a2dbc33
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-01-07 21:34:06 +0900

    [Silver V] Title: 단어 나누기, Time: 100 ms, Memory: 10536 KB -BaekjoonHub

--- node.js/백준/Silver/1251. 단어 나누기/단어 나누기.js
+++ node.js/백준/Silver/1251. 단어 나누기/단어 나누기.js
@@ -7,27 +7,15 @@ let answer = "";
 
 let smallest = null;
 
-// 가능한 모든 두 지점 선택 (i, j)
-for (let i = 1; i < word.length - 1; i++) {
-  for (let j = i + 1; j < word.length; j++) {
-    // 세 부분으로 분할
-    const part1 = word.slice(0, i);
-    const part2 = word.slice(i, j);
-    const part3 = word.slice(j);
+for (let i = 0; i < word.length - 2; i++) {
+    const first = word.substring(0, i + 1).split("").reverse().join("");
+    for (let j = i+1; j < word.length - 1; j++) {
+        const second = word.substring(i + 1, j + 1).split("").reverse().join("");
+        const third = word.substring(j + 1).split("").reverse().join("");
 
-    // 각 부분을 뒤집기
-    const rev1 = part1.split('').reverse().join('');
-    const rev2 = part2.split('').reverse().join('');
-    const rev3 = part3.split('').reverse().join('');
-
-    // 합치기
-    const candidate = rev1 + rev2 + rev3;
-
-    // 사전순으로 비교하여 가장 작은 값 갱신
-    if (smallest === null || candidate < smallest) {
-      smallest = candidate;
+        if (!smallest || smallest > first + second + third) smallest = first + second + third;
     }
-  }
 }
 
 console.log(smallest);
+
