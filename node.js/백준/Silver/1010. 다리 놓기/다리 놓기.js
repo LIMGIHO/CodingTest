49a9ab3481a9bb1827fc933cfe8c7e24f2294816
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-01-16 23:09:06 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 10832 KB -BaekjoonHub

--- node.js/백준/Silver/1010. 다리 놓기/다리 놓기.js
+++ node.js/백준/Silver/1010. 다리 놓기/다리 놓기.js
@@ -2,28 +2,14 @@ const fs = require("fs");
 // let arr = fs.readFileSync('./dev/stdin', 'utf8').trim().split(/\r?\n/);
 let input = fs.readFileSync('./dev/stdin', 'utf8').trim().split(/\r?\n/);
 
-const TC = input.slice(1).map((line) => line.trim().split(' ').map(Number));
-
-function solution(N, M) {
-  const n = M;
-  const r = N;
-  const dp = Array.from({ length: n + 1 }, () => Array(r + 1).fill(0));
-
-  // nC0, nCn = 1로 초기화
-  for (let i = 1; i <= n; i++) {
-    dp[i][0] = 1;
-    dp[i][i] = 1;
+for (let i = 1; i < input.length; i++) {
+  const [k, n] = input[i].split(" ");
+  let dp = Array(+n+1);
+  dp[0] = 1n;
+  dp[1] = 1n;
+  for (let j = 2; j <= +n+1; j++) {
+    dp[j] = dp[j-1] * BigInt(j);
   }
-
-  for (let i = 2; i <= n; i++) {
-    for (let j = 1; j <= Math.floor(i / 2); j++) {
-      const combi = dp[i - 1][j - 1] + dp[i - 1][j];
-      dp[i][j] = combi;
-      dp[i][i - j] = combi;
-    }
-  }
-  return dp[n][r];
+  
+  console.log((dp[n] / (dp[k] * dp[n-k])).toString());
 }
-
-const result = TC.map((testCase) => solution(testCase[0], testCase[1]));
-console.log(result.join('\n'));
