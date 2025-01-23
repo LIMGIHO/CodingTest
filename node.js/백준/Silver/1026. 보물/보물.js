eddead607d72f66cdbdd56926ab4861d19ea769f
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-01-23 20:50:18 +0900

    [Silver IV] Title: 보물, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- node.js/백준/Silver/1026. 보물/보물.js
+++ node.js/백준/Silver/1026. 보물/보물.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 let input = fs.readFileSync('./dev/stdin', 'utf8').trim().split(/\r?\n/);
 let N = input[0];
-let A = input[1].split(" ").map(Number).sort((a,b) => a - b);
-let B = input[2].split(" ").map(Number).sort((a,b) => b - a);
+
+function mathSort(a,b){return Number(a)-Number(b)}
+let A = input[1].split(" ").map(Number).sort(mathSort);
+let B = input[2].split(" ").map(Number).sort(mathSort);
 
 let answer = 0;
 for (let i = 0; i < A.length; i++) {
-    answer += A[i] * B[i];
+    answer += A[i] * B[B.length - 1- i];
 }
 
 console.log(answer);
