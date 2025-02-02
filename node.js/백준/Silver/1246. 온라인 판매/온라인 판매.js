9e018ee051ee2cc28c6a302cf4126e66c8e2d693
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-02-02 14:02:33 +0900

    [Silver IV] Title: 온라인 판매, Time: 92 ms, Memory: 9728 KB -BaekjoonHub

--- node.js/백준/Silver/1246. 온라인 판매/온라인 판매.js
+++ node.js/백준/Silver/1246. 온라인 판매/온라인 판매.js
@@ -2,26 +2,19 @@ const fs = require("fs");
 // let input = fs.readFileSync('./dev/stdin', 'utf8').trim().split(/\r?\n|\s/);
 let input = fs.readFileSync('./dev/stdin', 'utf8').trim().split(/\r?\n/);
 let [N,M] = input.shift().split(" ").map(Number);
-let P = input.map(Number);
+let P = input.map(Number).sort((a,b) => a-b);
 
 let answer = 0;
-let price = 0;
-P.forEach(p => {
-    let sum = 0;
-    let count = N;
-    for (let i = 0; i < P.length; i++) {
-        if (P[i] >= p) {
-            sum += p;
-            count--;
-        }
+let max = 0
+for (let i = 0; i < M; i++) {
+    const count = Math.min(N, M - i);
+    const price = P[i];
+    const sum = price * count;
 
-        if (count === 0) break;
+    if (max < sum) {
+        answer = price + ' ' + sum;
+        max = sum;
     }
-    
-    if (answer < sum) {
-        price = p;
-        answer = sum;
-    }
-})
+}
 
-console.log(price + ' ' + answer);
+console.log(answer);
