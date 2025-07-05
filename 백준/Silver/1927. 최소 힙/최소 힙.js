99091c4c7abc59482c315c8d68ccc6f257c1d11d
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-07-05 17:38:52 +0900

    [Silver II] Title: 최소 힙, Time: 208 ms, Memory: 24188 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.js
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -13,91 +13,96 @@ const input = fs.readFileSync(inputPath)
   .split('\n');
 
 class MinHeap {
-    constructor() {
-        this.heap = [];
+  constructor() {
+    this.heap = [];
+  }
+
+  list() {
+    return this.heap;
+  }
+
+  push(val) {
+    this.heap.push(val);
+    this.heapifyUp();
+  }
+  
+  pop() {
+    if (this.heap.length === 0) return 0;
+    if (this.heap.length === 1) return this.heap.pop();
+    
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    const currentVal = this.heap[index];
+
+    while (index > 0) {
+      const parentIdx = Math.floor((index - 1) / 2);
+      const parentVal = this.heap[parentIdx];
+
+      if (parentVal > currentVal) {
+        this.heap[index] = parentVal;
+        this.heap[parentIdx] = currentVal;
+        index = parentIdx;
+      } else {
+        break;
+      }
     }
+  }
 
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp();
+  heapifyDown() {
+    let index = 0;
+
+    while (true) {
+      const leftChildIdx = index * 2 + 1;
+      const rightChildIdx = index * 2 + 2;
+      const currentVal = this.heap[index]
+      let smallestIdx = index;
+
+      if (this.heap[leftChildIdx] && this.heap[smallestIdx] > this.heap[leftChildIdx]) {
+        smallestIdx = leftChildIdx;
+      }
+
+      if (this.heap[rightChildIdx] && this.heap[smallestIdx] > this.heap[rightChildIdx]) {
+        smallestIdx = rightChildIdx;
+      }
+
+      if (smallestIdx !== index) {
+        this.heap[index] = this.heap[smallestIdx];
+        this.heap[smallestIdx] = currentVal;
+        index = smallestIdx;
+      } else {
+        break;
+      }
     }
-
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        const curVal = this.heap[index];
-
-        while (index > 0) {
-            let parentIdx = Math.floor((index - 1) / 2);
-            const parentVal = this.heap[parentIdx];
-
-            if (parentVal > curVal) {
-                [this.heap[parentIdx], this.heap[index]] = [curVal, parentVal];
-                index = parentIdx;
-            } else {
-                break;
-            }
-        }
-    }
-
-    heapifyDown() {
-
-        let index = 0;
-        
-        while (true) {
-            const leftChildIdx = index * 2 + 1;
-            const rightChildIdx = index * 2 + 2;
-            let smallestChildIdx = index;
-
-            // 왼쪽 자식이 존재하고 현재 값보다 작은지 확인
-            if (leftChildIdx < this.heap.length && this.heap[leftChildIdx] < this.heap[smallestChildIdx]) {
-                smallestChildIdx = leftChildIdx;
-            }
-
-            // 오른쪽 자식이 존재하고 가장 작은 값보다 작은지 확인
-            if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[smallestChildIdx]) {
-                smallestChildIdx = rightChildIdx;
-            }
-
-            // 더 이상 교환할 필요가 없으면 종료
-            if (smallestChildIdx === index) {
-                break;
-            }
-
-            // 값 교환
-            [this.heap[index], this.heap[smallestChildIdx]] = [this.heap[smallestChildIdx], this.heap[index]];
-            index = smallestChildIdx;
-        }   
-    }
-
-    pop() {
-        if (this.heap.length === 0) return 0;
-        if (this.heap.length === 1) return this.heap.pop();
-
-        const root = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown();
-        return root;
-    }
+  }
 }
 
-function solve(S) {
-    const n = Number(S[0]);
-    const nums = S.slice(1).map(Number);
-    const answer = [];
 
+function solve(S) {
+    // const n = Number(S[0]);
+    const nums = S.slice(1).map(Number);
+    const n = nums.length;
+    const answer = [];
+    
     const minHeap = new MinHeap();
 
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-
-        if (num === 0) {
-            answer.push(minHeap.pop());
-            continue;
-        }
-
-        minHeap.insert(num);
+    for (let i = 0; i < n; i++) {
+      const num = nums[i];
+      if (num === 0) {
+        answer.push(minHeap.pop());
+        // console.log(minHeap.list());
+      } else {
+        minHeap.push(num);
+      }
+      // console.log("in for : ", minHeap.list());
     }
-    
+
+
     console.log(answer.join('\n'));
 }
 
