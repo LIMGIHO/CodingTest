64eab070218b41dd8c178c77ae24de568821aba2
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-08-04 22:01:01 +0900

    [Silver II] Title: 이동하기, Time: 216 ms, Memory: 27252 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.js
+++ 백준/Silver/11048. 이동하기/이동하기.js
@@ -1,38 +1,40 @@
 const fs = require('fs');
-const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
-const input = fs.readFileSync(inputPath).toString().trim().split('\n');
+// const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
+
+const input = new Uint8Array(fs.fstatSync(0).size);
+fs.readSync(0, input);
+
+
+function Scanner() {
+	this.idx = 0;
+
+	this.getInt = () => {
+		let n = 0;
+		while (!(input[this.idx] & 0x10)) ++this.idx;
+		while (input[this.idx] & 0x10) {
+			n = n * 10 + (input[this.idx] & 0x0f);
+			++this.idx;
+		}
+		return n;
+	};
+}
 
 const solve = (input) => {
-    const [n, m] = input[0].split(' ').map(Number);
-    const maze = input.splice(1).map(m => m.split(' ').map(Number));
-    
-    // const dfs = (cx, cy, candies) => {
-    //     if (cx === n-1 && cy === m-1) {
-    //         answer = Math.max(answer, candies);
-    //         return;
-    //     };
+    // const [n, m] = input[0].split(' ').map(Number);
+    // const maze = input.splice(1).map(m => m.split(' ').map(Number));
 
-    //     for (const [dx, dy] of direct) {
-    //         const nx = cx + dx;
-    //         const ny = cy + dy;
-                        
-    //         if (nx < n && ny < m) {    
-    //             dfs(nx, ny, candies + maze[nx][ny]);
-    //         }
-    //     }
-    // }
-    // dfs(0,0,maze[0][0]);
+    const sc = new Scanner();
+    const n = sc.getInt();
+    const m = sc.getInt();
 
     const dp = Array.from({length:n+1}, () => Array(m+1).fill(0));
     
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= m; j++) {
-            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + maze[i-1][j-1];
+            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + sc.getInt()//maze[i-1][j-1];
         }
     }
     return dp[n][m];
 }
 
 console.log(solve(input));
-
-
