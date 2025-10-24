51b66fdcab7f90d17b7dbb28bb2026c0d0a63b0f
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-10-24 12:53:31 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 88 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
+const input = fs.readFileSync(inputPath).toString().trim().split(/\r?\n/);
+
+const solve = (input) => {
+  return "고려대학교";
+}
+
+console.log(solve(input));
