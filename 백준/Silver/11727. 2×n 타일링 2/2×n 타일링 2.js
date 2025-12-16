a95f540e8a3942804b45b1a046a88b7167d2b2aa
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2025-12-16 12:50:13 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 152 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
+const input = fs.readFileSync(inputPath).toString().trim().split('\n');
+
+const solve = (input) => {
+    const N = Number(input[0]);
+    const dp = Array.from({length:N+1}, () => 0);
+    dp[1] = 1;
+    dp[2] = 3;
+
+    for (let i = 3; i <= N; i++) {
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+    }
+
+    return dp[N];
+}
+
+console.log(solve(input));
