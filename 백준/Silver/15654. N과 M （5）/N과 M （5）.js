76fa232af2260bc7ee63fa40e8882d4f9eb4b5cb
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2026-01-12 12:30:46 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 22604 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
+const input = fs.readFileSync(inputPath).toString().trim().split('\n');
+
+const solve = (input) => {
+    const [N,M] = input[0].split(' ').map(Number);
+    const nums = input[1].split(' ').map(Number).sort((a,b) => a-b);
+ 
+    const answer = [];
+    const temp = Array.from({length: M});
+    const check = Array.from({length:N+1}, () => false);
+    const dfs = (depth = 0) => {
+        if (depth === M) {
+            answer.push(temp.join(' ').trim());
+            return;
+        }
+
+        for (let i = 0; i < N; i++) {
+            if (check[i]) continue;
+
+            check[i] = true;
+            temp[depth] = nums[i];
+            dfs(depth+1);
+            check[i] = false;
+        }
+    }
+
+    dfs();
+    
+    return answer.join('\n');
+}
+
+console.log(solve(input));
