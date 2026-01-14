3142946a2bbf668e179baa082aa857b1add1a060
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2026-01-15 08:51:39 +0900

    [Silver III] Title: N과 M (8), Time: 152 ms, Memory: 12608 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
+const input = fs.readFileSync(inputPath).toString().trim().split('\n');
+
+const solve = (input) => {
+    const [N,M] = input[0].split(' ').map(Number);
+    const nums = input[1].split(' ').map(Number).sort((a,b) => a-b);
+ 
+    const answer = [];
+    const temp = Array.from({length: M});
+    const check = Array.from({length:N}, () => false);
+    const dfs = (start, depth = 0) => {
+        if (depth === M) {
+            answer.push(temp.join(' '));
+            return;
+        }
+
+        for (let i = start; i < N; i++) {
+            // if (check[i]) continue;
+
+            // check[i] = true;
+            temp[depth] = nums[i];
+            dfs(i, depth+1);
+            // check[i] = false;
+        }
+    }
+
+    dfs(0);
+    
+    return answer.join('\n');
+}
+
+console.log(solve(input));
