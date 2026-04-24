01c55b5478f1886cc9fdf7b6854d128039f33f92
LIMGIHO <59502715+LIMGIHO@users.noreply.github.com>
2026-04-24 12:42:32 +0900

    [Silver V] Title: 반지, Time: 100 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.js
+++ 백준/Silver/5555. 반지/반지.js
@@ -1,22 +1,12 @@
-const fs = require('fs');
-const inputPath = fs.existsSync('dev/stdin') ? 'dev/stdin' : '/dev/stdin';
-const input = fs.readFileSync(inputPath).toString().trim().split(/\r?\n/);
-
-const solve = (input) => {
-    let t = 0;
-    const target = input[t++];
-    const N = Number(input[t++]);
-
-    let answer = 0;
-    for (let i = 0; i < N; i++) {
-        const str = input[t] + input[t++];
-
-        if (str.includes(target))
-            answer++;
-    }
-
-    return answer;
-}
-
-console.log(solve(input));
-
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
